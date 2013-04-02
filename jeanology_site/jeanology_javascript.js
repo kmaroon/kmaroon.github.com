53d2992ed23ea1d0d775e3e9e00264f16f000d48
kim maroon <kmaroonfoto@gmail.com>
2013-04-01 21:58:41 -0400

    added new changes

---
+++ jeanology_site/jeanology_javascript.js
@@ -0,0 +1,85 @@
+
+$(document).ready(function() {
+    var scrollorama = $.scrollorama({
+        blocks:'.scrollblock'
+    });
+    
+
+	scrollorama.animate("#infobox_1853", {
+    	duration:400, property: 'left', delay:120, start:500, end:100
+    	
+    });
+    
+    scrollorama.animate("#photocontainer_1853", {
+    	duration:400, property: 'top', delay:120, start:400, end:0
+    	
+    });
+    
+    scrollorama.animate("#infobox_1870", {
+    	duration:600, property: 'left', delay:600, start:-400, end:100
+    	
+    });
+    
+    scrollorama.animate("#photocontainer_1870", {
+    	duration:600, property: 'top', delay:600, start:400, end:0
+    	
+    });
+    
+    scrollorama.animate("#infobox_1872", {
+    	duration:600, property: 'left', delay:1270, start:400, end:100
+    	
+    });
+    
+    scrollorama.animate("#photocontainer_1872", {
+    	duration:600, property: 'top', delay:1270, start:400, end:0
+    	
+    });
+    
+    scrollorama.animate("#infobox_1873", {
+    	duration:600, property: 'left', delay:1940, start:-400, end:100
+    	
+    });
+    
+    scrollorama.animate("#photocontainer_1873", {
+    	duration:600, property: 'top', delay:1940, start:400, end:0
+    	
+    });
+    
+    scrollorama.animate("#infobox_1900", {
+    	duration:900, property: 'left', delay:2300, start:400, end:100
+    	
+    });
+    
+    scrollorama.animate("#photocontainer_1900", {
+    	duration:900, property: 'top', delay:2300, start:400, end:0
+    	
+    });
+    
+     scrollorama.animate("#infobox_1904", {
+    	duration:900, property: 'left', delay:2970, start:-400, end:100
+    	
+    });
+    
+    scrollorama.animate("#infobox_1940", {
+    	duration:900, property: 'left', delay:3630, start:400, end:100
+    	
+    });
+    
+    scrollorama.animate("#photocontainer_1940", {
+    	duration:500, property: 'top', delay: 4000, start:400, end:0
+    	
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});
