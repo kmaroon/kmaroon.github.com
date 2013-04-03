e58bbde98fa8fcb46eea214caaa93760341df2d2
Massart User <massartuser@T-306-12.local>
2013-04-03 14:07:05 -0400

    added all the new files

---
+++ jeanology_site/jeanology_javascript.js
@@ -0,0 +1,89 @@
+$(document).ready(function() {
+    var scrollorama = $.scrollorama({
+        blocks:'.scrollblock'
+    });
+    
+
+	scrollorama.animate("#infobox_1853", {
+    	duration:400, property: 'left', delay:120, start:-1000, end:100
+
+    });
+    
+    scrollorama.animate("#photocontainer_1853", {
+    	duration:400, property: 'top', delay:120, start:400, end:0
+    	
+    });
+    
+    scrollorama.animate("#infobox_1870", {
+    	duration:600, property: 'left', delay:600, start:-1000, end:100
+    	
+    });
+    
+    scrollorama.animate("#photocontainer_1870", {
+    	duration:600, property: 'top', delay:600, start:500, end:0
+    	
+    });
+    
+    scrollorama.animate("#infobox_1872", {
+    	duration:600, property: 'left', delay:1270, start:-1000, end:100
+    	
+    });
+    
+    scrollorama.animate("#photocontainer_1872", {
+    	duration:600, property: 'top', delay:1270, start:500, end:0
+    	
+    });
+    
+    scrollorama.animate("#infobox_1873", {
+    	duration:600, property: 'left', delay:1940, start:-1000, end:100
+    	
+    });
+    
+    scrollorama.animate("#photocontainer_1873", {
+    	duration:600, property: 'top', delay:1940, start:500, end:0
+    	
+    });
+    
+    scrollorama.animate("#infobox_1900", {
+    	duration:900, property: 'left', delay:2200, start:-1000, end:100
+    	
+    });
+    
+    scrollorama.animate("#photocontainer_1900", {
+    	duration:900, property: 'top', delay:2220, start:500, end:0
+    	
+    });
+    
+     scrollorama.animate("#infobox_1904", {
+    	duration:900, property: 'left', delay:2970, start:-1000, end:100
+    	
+    });
+    
+    scrollorama.animate("#photocontainer_1904", {
+    	duration:900, property: 'top', delay:2980, start:500, end:0
+    	
+    });
+    
+    scrollorama.animate("#infobox_1940", {
+    	duration:900, property: 'left', delay:3600, start:-1000, end:100
+    	
+    });
+    
+    scrollorama.animate("#photocontainer_1940", {
+    	duration:600, property: 'top', delay: 3900, start:500, end:0
+    	
+    });
+    
+    scrollorama.animate("#titletext", {
+    	duration:300, property: 'opacity', delay:15, start:1, end:0
+    	
+    });
+
+    
+});
+
+$('#jacob').hover(function() {
+ 
+$('#jacobdavistext').css('visibility','visible');﻿
+ 
+});
